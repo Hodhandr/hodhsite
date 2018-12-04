f90621c644e5f8874aff555209b8acf538a85bc3
Stian Aleksander Sætrang <stianas99@gmail.com>
2018-12-04 15:09:09 +0100

    +Implemented basic SPA load feature

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,1 +1,41 @@
-console.log("JS file loaded!");
+/** Container class for main HodhSite script */
+class HodhScript {
+  constructor() {
+    console.log("JS file loaded!");
+
+    this.pageOut = document.getElementById("page-out");
+
+    if (typeof this.pageOut === "undefined") {
+      console.error(
+        "Something has gone terribly wrong and we can't find the #page-out element!"
+      );
+      return;
+    }
+
+    this.loadPage("home");
+  }
+
+  /** Loads page by name. */
+  async loadPage(pgName) {
+    const fetchRes = await fetch(`pages/${pgName}.html`);
+    if (!fetchRes.ok) {
+      console.error(
+        "HodhScript.loadPage failed for",
+        pgName,
+        "expected a string to a valid page."
+      );
+      return;
+    }
+
+    const text = await fetchRes.text();
+    this.pageOut.innerHTML = text;
+    console.log(fetchRes, text);
+  }
+}
+//Create instance
+const hodhScript = new HodhScript();
+
+/** Awaitable (promise-ified) 'setTimeout(milliseconds)' */
+function wait(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
